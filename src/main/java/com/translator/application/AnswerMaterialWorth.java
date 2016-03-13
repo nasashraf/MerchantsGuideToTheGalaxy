aea872e89f387a02d76f039e10dfd6db68353a0e
Nas Ashraf <nas.ashraf@hotmail.com>
2016-03-13 13:39:33 +0000

    Added MaterialCost which better supports concept of cost calculation

--- src/main/java/com/translator/application/AnswerMaterialWorth.java
+++ src/main/java/com/translator/application/AnswerMaterialWorth.java
@@ -1,13 +1,17 @@
 package com.translator.application;
 
 import com.translator.domain.model.calculator.Credits;
-import com.translator.domain.model.numeral.*;
+import com.translator.domain.model.numeral.Cost;
+import com.translator.domain.model.numeral.Material;
+import com.translator.domain.model.numeral.MaterialCost;
+import com.translator.domain.model.numeral.RomanNumeral;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static com.translator.domain.model.numeral.MaterialOperation.MULTIPLY;
+//import static com.translator.application.AnswerMaterialWorth.CostBuilder.romanNumerals;
 
 public class AnswerMaterialWorth extends AbstractAnsweringService {
 
@@ -26,17 +30,12 @@ public class AnswerMaterialWorth extends AbstractAnsweringService {
 
         try {
             String quantityAndMaterialText = extractQuestionDetails(question);
-
             Material material = createMaterial(quantityAndMaterialText);
+            List<RomanNumeral> numerals = romanNumeralsFrom(quantitiesTextFrom(quantityAndMaterialText));
 
-            String quantitiesText = quantitiesTextFrom(quantityAndMaterialText);
-            List<Cost> costs = new ArrayList<Cost>();
-            costs.addAll(romanNumeralsFrom(quantitiesText));
-            costs.add(new MaterialCost(material, MULTIPLY));
+            Credits worth = creditsCalculator().calculate(CostBuilder.romanNumerals(numerals).multipliedByCostOf(material));
 
-            Credits worth = creditsCalculator().calculate(costs);
-
-            answer = answerText(quantitiesText, material.name(), worth.amount());
+            answer = answerText(quantitiesTextFrom(quantityAndMaterialText), material.name(), worth.amount());
         } catch (TranslationException te) {
             answer = "I have no idea what you are talking about";
         }
@@ -71,5 +70,25 @@ public class AnswerMaterialWorth extends AbstractAnsweringService {
 
     protected String answerText(String quantities, String materialName, String worthAmount) {
         return quantities + " " + materialName + " is " + worthAmount + " Credits";
+    }
+
+
+    private static class CostBuilder {
+        private List<Cost> costs;
+
+        public CostBuilder(List<RomanNumeral> materialName) {
+            costs = new ArrayList<Cost>();
+            costs.addAll(materialName);
+        }
+
+        public static CostBuilder romanNumerals(List<RomanNumeral> materialName) {
+            return new CostBuilder(materialName);
+        }
+
+        public List<Cost> multipliedByCostOf(Material material) {
+            MaterialCost materialCost = MaterialCost.aMaterialCost(material, MULTIPLY);
+            costs.add(materialCost);
+            return costs;
+        }
     }
 }
